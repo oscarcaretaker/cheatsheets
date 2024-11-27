696293c79fccaefa3886325e9cc1dfe5face3b15
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2024-11-27 20:47:37 +0530

    New

--- PYTHON.py
+++ PYTHON.py
@@ -3,3 +3,11 @@
 # map() - apply to each element of container.
 # filter() - check all element of container for specific function.
 # reduce() - to apply sequential function to elements.
+
+
+#*************************************************************************
+
+# ACCESS CONTROL IN PYTHON
+#  var -> Public
+#  _var -> Protected
+#  __var -> Private
