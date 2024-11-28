d961c8a1721da51ef383a541ea2bca3e33ed568e
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2024-11-28 17:50:54 +0530

    Latest

--- PYTHON.py
+++ PYTHON.py
@@ -5,9 +5,19 @@
 # reduce() - to apply sequential function to elements.
 
 
-#*************************************************************************
+# ------------------------------------------------------------ #
 
 # ACCESS CONTROL IN PYTHON
 #  var -> Public
 #  _var -> Protected
 #  __var -> Private
+
+# ------------------------------------------------------------- #
+
+# OPERATOR OVERLOADING
+# __add__ = +
+# __sub__ = -
+# __mul__ = *
+# __truediv__ = /
+# __eq__ = ==
+# __ne__ = !=
